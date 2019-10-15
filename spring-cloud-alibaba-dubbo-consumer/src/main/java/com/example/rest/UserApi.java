f9a62f4f0e5437e22d0ce939c1a61c631787a7c1
liuzijian <liuzijian@m-tour.com.cn>
2019-10-15 19:34:26 +0800

    update

--- spring-cloud-alibaba-dubbo-consumer/src/main/java/com/example/rest/UserApi.java
+++ spring-cloud-alibaba-dubbo-consumer/src/main/java/com/example/rest/UserApi.java
@@ -10,7 +10,7 @@ import com.example.servlce.UserService;
 @RestController
 public class UserApi {
     
-    @Reference
+    @Reference(version = "1.0.0")
     private UserService userService;
     
     @RequestMapping("get")
--- spring-cloud-alibaba-dubbo-provider/src/main/java/com/example/servlce/impl/UserServiceImpl.java
+++ spring-cloud-alibaba-dubbo-provider/src/main/java/com/example/servlce/impl/UserServiceImpl.java
@@ -5,16 +5,19 @@ import com.example.dto.UserReq;
 import com.example.dto.UserResp;
 import org.apache.dubbo.config.annotation.Service;
 import com.example.servlce.UserService;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service(version = "1.0.0")
 public class UserServiceImpl implements UserService {
-
+    @Value("${dubbo.protocol.port}")
+    private String port;
+    
     @Override
     public UserResp getUsernameById(UserReq req) {
         UserResp resp = new UserResp();
         resp.setUser(new User());
         resp.setCode(200);
-        resp.setMessage("调用成功");
+        resp.setMessage(port + "调用成功");
         return resp;
     }
 }
